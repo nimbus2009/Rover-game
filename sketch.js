79c217f5cf7cd8ce99c9ae86a9b3764ead60735c
Anuj M Doddakaragi <46996067+celestial74@users.noreply.github.com>
2021-06-22 18:56:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -203,7 +203,7 @@ function draw() {
 
         score=0;
 
-        if(mousePressedOver(start)) {
+        if(mousePressedOver(start)||touches[0]==random(start.x-10,start.x+10)) {
             state="play";
             start.visible=false;
             rules.visible=false;
